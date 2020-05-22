5a662b02c9a1d68852f9b9a00f43ec16d7c029c0
Amazix-0010 <65762218+Amazix-0010@users.noreply.github.com>
2020-05-22 12:03:55 +0200

    A
    A

---
+++ Assets/Scripts/Bullet.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    public float speed = 8f;
+
+    public Rigidbody theRB;
+
+    public float lifeTime = 2f;
+
+    public GameObject bulletparticle;
+
+    public int damage;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        theRB.velocity = transform.forward * speed;
+
+        lifeTime -= Time.deltaTime;
+        if(lifeTime < 0)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Enemy")
+        {
+            // Destroy(other.gameObject);
+
+            other.gameObject.GetComponent<EnemyHealth>().DamageEnemy(damage);
+        }
+               
+            Destroy(gameObject);
+            Instantiate(bulletparticle, transform.position + (transform.forward * (-speed * Time.deltaTime)), transform.rotation);
+        
+    }
+}
---
+++ Assets/Scripts/EnemyController.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyController : MonoBehaviour
+{
+    public float moveSpeed;
+
+    public Rigidbody theRB;
+
+    public bool chasing;
+
+    public float distanceToChase = 10f, distanceToLose = 15f;
+
+    private Vector3 targetPoint, startPoint;
+
+    public NavMeshAgent agent;
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        startPoint = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        targetPoint = Player.instance.transform.position;
+        targetPoint.y = transform.position.y;
+
+        if (!chasing)
+        {
+            if (Vector3.Distance(transform.position,targetPoint) < distanceToChase)
+            {
+                chasing = true;
+
+            }
+        }
+        else
+        {
+            // transform.LookAt(targetPoint);
+            // theRB.velocity = transform.forward * moveSpeed;
+
+            agent.destination = targetPoint;
+
+            if (Vector3.Distance(transform.position,targetPoint) > distanceToLose)
+            {
+                chasing = false;
+
+
+                agent.destination = startPoint;
+            }
+        }
+    }
+}
