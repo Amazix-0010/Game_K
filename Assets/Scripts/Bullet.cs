3cd03fb05206e9392221efec1e925dbd2c5f778b
Amazix-0010 <65762218+Amazix-0010@users.noreply.github.com>
2020-05-22 17:46:59 +0200

    Bullet

--- Assets/Scripts/Bullet.cs
+++ Assets/Scripts/Bullet.cs
@@ -14,6 +14,8 @@ public class Bullet : MonoBehaviour
 
     public int damage;
 
+    public bool damageEnemy, damagePlayer;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -34,11 +36,16 @@ public class Bullet : MonoBehaviour
 
     private void OnTriggerEnter(Collider other)
     {
-        if (other.gameObject.tag == "Enemy")
+        if (other.gameObject.tag == "Enemy" && damageEnemy == true)
         {
             // Destroy(other.gameObject);
 
             other.gameObject.GetComponent<EnemyHealth>().DamageEnemy(damage);
+        }
+
+        if(other.gameObject.tag == "Player " && damagePlayer == true)
+        {
+            Debug.Log("Zásah hráče " + transform.position);
         }
                
             Destroy(gameObject);
